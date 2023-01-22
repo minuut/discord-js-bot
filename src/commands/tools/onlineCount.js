ac03510f2f895025713afe40ab142365d065756b
Yonnie <yonnie.kauwenberg.bee@outlook.com>
2023-01-22 22:29:23 +0100

    Add files via upload

---
+++ src/commands/tools/onlineCount.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder() 
+    .setName('onlineCount')
+    .setDescription('return online count from API'),
+
+    async execute(message, args, rest) {
+        try {                             //   put your api url here
+            const response = await axios.get('                      ');
+            const data = response.data;
+            const dataString = JSON.stringify(data);
+            // extract the number using a regular expression
+            const number = dataString.match(/\d+/);  
+            const customString = 'users are currently online';
+            const finalString = `The online count is ${number} ${customString}`;
+            await message.channel.send(finalString);
+        } catch (error) {
+            console.error(error);
+            await message.channel.send('An error occurred while fetching the data.');
+        }
+    }
+}
