84b685392fcabebe5d00e9564fbaffb348d1208f
yon <70378641+minuut@users.noreply.github.com>
2023-03-07 22:05:07 +0100

    debug
    fixed error message

--- src/commands/tools/online.js
+++ src/commands/tools/online.js
@@ -3,21 +3,27 @@ const axios = require('axios');
 
 module.exports = {
     data: new SlashCommandBuilder() 
-    .setName('online')
-    .setDescription('return online count API'),
+        .setName('online')
+        .setDescription('Return online count from ProHotel API'),
 
-    async execute(message) {
-        try {   
-            const response = await axios.get('(your api url here)', { timeout: 5000 });
-            const data = response.data;
-            const dataString = JSON.stringify(data);
-            const number = dataString.match(/\d+/);  
-            const customString = 'users currently online';
-            const finalString = `There are ${number} ${customString}`;
-            await message.channel.send(finalString);
-        } catch (error) {
-            console.error(error);
-            await message.channel.send('An error occurred while fetching the data.');
-        }
-    }
+        async execute(interaction) {
+            try {   
+              const response = await axios.get('https://{website}.nl/api/online-count');
+              const data = response.data;
+              const dataString = JSON.stringify(data);
+              // extract the number using a regular expression
+              const number = dataString.match(/\d+/);  
+              const customString = 'users currently online';
+              const finalString = `There are ${number} ${customString}`;
+          
+              // Defer reply before sending message
+              await interaction.deferReply();
+          
+              await interaction.followUp(finalString);
+            } catch (error) {
+              console.error(error);
+              await interaction.followUp('An error occurred while fetching the data.');
+            }
+          }
+          
 }
